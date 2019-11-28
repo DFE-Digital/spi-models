5f35ae7d7394f41888274803388cb6cc391e3aa2
Matt Middleton <matt.middleton@mtcs.org.uk>
2019-11-28 13:54:37 +0000

    Initial project structure, and initial rough models.

---
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/LearningProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Dfe.Spi.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A learning provider.
+    /// </summary>
+    public class LearningProvider : ModelsBase
+    {
+        /// <summary>
+        /// Gets or sets a set of <see cref="Governer" /> instances.
+        /// </summary>
+        public IEnumerable<Governer> Governers
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/ModelsBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Dfe.Spi.Models
+{
+    /// <summary>
+    /// Abstract base class for all models in the library.
+    /// </summary>
+    public class ModelsBase
+    {
+        /// <summary>
+        /// Gets or sets the name of the entity.
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
