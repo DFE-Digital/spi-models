20272041dac0aedf67ba1b47606b7a70e2045fca
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-02-07 10:35:18 +0000

    Just ordering what we've got before updating.

--- src/Dfe.Spi.Models/Dfe.Spi.Models/LearningProvider.cs
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/LearningProvider.cs
@@ -9,35 +9,9 @@
     public class LearningProvider : ModelsBase
     {
         /// <summary>
-        /// Gets or sets a set of <see cref="Governor" /> instances.
+        /// Gets or sets the Academy Trust code of the entity.
         /// </summary>
-        public IEnumerable<Governor> Governors { get; set; }
-
-        /// <summary>
-        /// Gets or sets the legal name of the entity.
-        /// </summary>
-        public string LegalName { get; set; }
-
-        
-        /// <summary>
-        /// Gets or sets the URN of the entity.
-        /// </summary>
-        public long? Urn { get; set; }
-
-        /// <summary>
-        /// Gets or sets the UKPRN of the entity.
-        /// </summary>
-        public long? Ukprn { get; set; }
-
-        /// <summary>
-        /// Gets or sets the UPRN of the entity.
-        /// </summary>
-        public string Uprn { get; set; }
-
-        /// <summary>
-        /// Gets or sets the Companies House number of the entity.
-        /// </summary>
-        public string CompaniesHouseNumber { get; set; }
+        public string AcademyTrustCode { get; set; }
 
         /// <summary>
         /// Gets or sets the Charities Commission number of the entity.
@@ -45,9 +19,14 @@
         public string CharitiesCommissionNumber { get; set; }
 
         /// <summary>
-        /// Gets or sets the Academy Trust code of the entity.
+        /// Gets or sets the CloseDate of the entity.
         /// </summary>
-        public string AcademyTrustCode { get; set; }
+        public DateTime? CloseDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Companies House number of the entity.
+        /// </summary>
+        public string CompaniesHouseNumber { get; set; }
 
         /// <summary>
         /// Gets or sets the DfE number of the entity.
@@ -60,31 +39,14 @@
         public string EstablishmentNumber { get; set; }
 
         /// <summary>
-        /// Gets or sets the previous establishment number of the entity.
+        /// Gets or sets a set of <see cref="Governor" /> instances.
         /// </summary>
-        public string PreviousEstablishmentNumber { get; set; }
-        
-        
+        public IEnumerable<Governor> Governors { get; set; }
 
         /// <summary>
-        /// Gets or sets the Postcode of the entity.
+        /// Gets or sets the legal name of the entity.
         /// </summary>
-        public string Postcode { get; set; }
-
-        /// <summary>
-        /// Gets or sets the Status of the entity.
-        /// </summary>
-        public string Status { get; set; }
-
-        /// <summary>
-        /// Gets or sets the Type of the entity.
-        /// </summary>
-        public string Type { get; set; }
-
-        /// <summary>
-        /// Gets or sets the SubType of the entity.
-        /// </summary>
-        public string SubType { get; set; }
+        public string LegalName { get; set; }
 
         /// <summary>
         /// Gets or sets the OpenDate of the entity.
@@ -92,8 +54,43 @@
         public DateTime? OpenDate { get; set; }
 
         /// <summary>
-        /// Gets or sets the CloseDate of the entity.
+        /// Gets or sets the Postcode of the entity.
         /// </summary>
-        public DateTime? CloseDate { get; set; }
+        public string Postcode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the previous establishment number of the entity.
+        /// </summary>
+        public string PreviousEstablishmentNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Status of the entity.
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SubType of the entity.
+        /// </summary>
+        public string SubType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Type of the entity.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UKPRN of the entity.
+        /// </summary>
+        public long? Ukprn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UPRN of the entity.
+        /// </summary>
+        public string Uprn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URN of the entity.
+        /// </summary>
+        public long? Urn { get; set; }
     }
 }
