81af89acd1b6f19a08f97d6ca835f375d0f449c4
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-02-18 09:33:17 +0000

    Bit o' a restructure around models, entities and embedded types in preperation for lineage.

---
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/Address.cs
@@ -0,0 +1,33 @@
+﻿namespace Dfe.Spi.Models
+{
+    /// <summary>
+    /// A (snail mail) address.
+    /// </summary>
+    public class Address : ModelsBase
+    {
+        /// <summary>
+        /// Gets or sets the first address line.
+        /// </summary>
+        public string AddressLine1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second address line.
+        /// </summary>
+        public string AddressLine2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the third address line.
+        /// </summary>
+        public string AddressLine3 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the town.
+        /// </summary>
+        public string Town { get; set; }
+
+        /// <summary>
+        /// Gets or sets the county.
+        /// </summary>
+        public string County { get; set; }
+    }
+}
---
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/Entities/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Dfe.Spi.Models.Entities
+{
+    /// <summary>
+    /// Base class for which all top-level entities to derive from.
+    /// </summary>
+    public abstract class EntityBase : ModelsBase
+    {
+        /// <summary>
+        /// Gets or sets the name of the entity.
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
