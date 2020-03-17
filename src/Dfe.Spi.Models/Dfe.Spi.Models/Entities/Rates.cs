0042a1039c6590c384354fe7f0665ce44b7f6a9a
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-03-17 09:41:37 +0000

    Initial rates object.

---
+++ src/Dfe.Spi.Models/Dfe.Spi.Models/Entities/Rates.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfe.Spi.Models.Entities
+{
+    /// <summary>
+    /// Contains information about rates.
+    /// </summary>
+    public class Rates : EntityBase
+    {
+        // Nothing for now.
+    }
+}
